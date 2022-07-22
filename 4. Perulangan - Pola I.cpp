b33edb9f10b0bc1dcec34d72adf71220ed08ddcf
mindofkaka <106789262+mindofkaka@users.noreply.github.com>
2022-07-22 08:33:25 +0700

    Create 4. Perulangan - Pola I.cpp

---
+++ 4. Perulangan - Pola I.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    
+    cin >> a >> b;
+    
+    for (int i = 1; i <= a; i++){
+        if (i % b == 0){
+            cout << "*" << " ";
+        } else {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    
+    return 0;
+}
