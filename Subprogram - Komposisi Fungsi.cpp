16ac939bad7a8eb702fa2033f646b81ec76e7767
mindofkaka <106789262+mindofkaka@users.noreply.github.com>
2022-08-15 10:00:42 +0700

    Create Subprogram - Komposisi Fungsi.cpp

---
+++ Subprogram - Komposisi Fungsi.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int reverse(int x) {
+    int temp = x;
+    int ret  = 0;
+
+    while (temp > 0) {
+        ret  = (ret*10) + (temp%10);
+        temp = temp/10;
+    }
+    return ret;
+}
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    
+    int c = reverse(a) + reverse(b);
+    
+    cout << reverse(c);
+}
+
+
+
