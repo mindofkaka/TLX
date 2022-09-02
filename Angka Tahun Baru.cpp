41d9ad4e314a259c84a932eeaafd3118980d90bd
mindofkaka <106789262+mindofkaka@users.noreply.github.com>
2022-09-02 08:38:53 +0700

    Add files via upload

---
+++ Angka Tahun Baru.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n; cin >> n;
+	if(n==2 || n==3 || n==5) cout << "YES";
+	else cout << "NO";
+}
