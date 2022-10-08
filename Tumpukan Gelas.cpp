b39578b946ede477b81a37d81525fefc8fcbf3ca
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2022-10-08 13:12:19 +0700

    Add files via upload

---
+++ Tumpukan Gelas.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    if(n%2==0) cout << "Tidak";
+    else cout << "Perlu";
+}
