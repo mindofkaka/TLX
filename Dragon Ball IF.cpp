0cd61d23b3d1a334362664a94206c832f8e585ff
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2022-11-03 16:19:18 +0700

    Add files via upload

---
+++ Dragon Ball IF.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while(t--){
+		int n; cin >> n;
+		cout << 'k';
+		for(int i = 0; i < n; i++) cout << 'a';
+		cout << 'm';
+		for(int i = 0; i < n; i++) cout << 'e';
+		cout << 'h';
+		for(int i = 0; i < n; i++) cout << 'a';
+		cout << 'm';
+		for(int i = 0; i < n; i++) cout << 'e';
+		cout << 'h';
+		for(int i = 0; i < n; i++) cout << 'a';
+		cout << "\n";
+	}
+}
