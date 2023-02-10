742ea9a624a1a1a1ac2e061ff19ab61b5aae95e3
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2023-02-10 15:15:15 +0700

    Update Mengangkut Bebek.cpp

--- Mengangkut Bebek.cpp
+++ Mengangkut Bebek.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
+ll p[1005], c[1005], b[100005];
 int main() {
-	vector< pair<int,int> > arr;
-	int k; cin >> k;
-	for(int i = 0; i < k; i++) cin >> arr[i].second;
-	for(int i = 0; i < k; i++) cin >> arr[i].first;
-	int n; cin >> n;
-	int b[n+1];
+	ll k; cin >> k;
+	for(int i = 0; i < k; i++) cin >> p[i];
+	for(int i = 0; i < k; i++) cin >> c[i];
+	ll n; cin >> n;
 	for(int i = 0; i < n; i++) cin >> b[i];
-	sort(b, b+n);
-	sort(arr.begin(), arr.end());
-	int ind = 0, ans = 0;
+	vector<pair<ll, ll>> v;
+	for(int i = 0; i < k; i++) v.push_back({c[i], p[i]});
+	sort(v.begin(), v.end());
+	sort(b, b+n, greater<int>());
+	ll idx = 0, ans = 0;
 	for(int i = 0; i < n; i++){
-		if(arr[ind].second <= 0) ind++;
-		arr[ind].second--;
-		ans += v[ind].first*b[i];
+		if(v[idx].second <= 0) idx++;
+		v[idx].second--;
+		ans+=v[idx].first*b[i];
 	}
 	cout << ans;
 }
