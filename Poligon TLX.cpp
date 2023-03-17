100847ffa4870fbcc3e1379ae6c5eddba12de06d
Muhammad Ludaka Firdaus <106789262+mindofkaka@users.noreply.github.com>
2023-03-17 15:06:51 +0700

    Add files via upload

---
+++ Poligon TLX.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	long long n, m;
+	cin >> n >> m;
+	cout << 2*min(n, m) << '\n';
+}
+
+int main() {
+	int t; cin >> t;
+	while(t--){
+		solve();
+	}
+}
